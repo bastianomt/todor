0596b91d7bbcc96391bbf4c74db228fbc62cdeb3
sebas <bastianomoret@gmail.com>
2024-12-11 13:22:37 +0100

    new files added: - added panel control - added login - added edit todos

--- todor/models.py
+++ todor/models.py
@@ -3,23 +3,23 @@ from todor import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.Text(20), nullable=False)
-
+    password = db.Column(db.Text, nullable=False)
+    
     def __init__(self, username, password):
-        self.username = username,
+        self.username = username
         self.password = password
     
     def __repr__(self):
-        return f'<User: {self.username} >'
+        return f'<User: {self.username}>'
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     created_By = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     title = db.Column(db.String(100), nullable=False)
-    desc = db.Column(db.String(200))
+    desc = db.Column(db.Text)
     state = db.Column(db.Boolean, default=False)
 
-    def __init__(self, created_By, title, desc, state):
+    def __init__(self, created_By, title, desc, state=False):
         self.created_By = created_By,
         self.title = title,
         self.desc = desc,
